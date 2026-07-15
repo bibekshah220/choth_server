a32a5257ce99ff5782f44685e31556d7546d3ca4
bibekshah220 <bibekshah425@gmail.com>
2026-07-15 22:54:35 +0545

    feat: configure Cloudinary for image management

---
+++ src/config/cloudinary.config.ts
@@ -0,0 +1,10 @@
+import { v2 as cloudinary } from "cloudinary";
+import { cloudinary_config } from "./config.js";
+
+cloudinary.config({
+  cloud_name: cloudinary_config.cloud_name,
+  api_key: cloudinary_config.api_key,
+  api_secret: cloudinary_config.api_secret,
+});
+
+export default cloudinary;
