3a87183b6bc83b7b11b6aee98f29e60cd4c965a7
bibekshah220 <bibekshah425@gmail.com>
2026-07-15 22:54:44 +0545

    feat: refactor Cloudinary configuration to streamline upload functionality

--- src/utils/cloudinary.utils.ts
+++ src/utils/cloudinary.utils.ts
@@ -1,11 +1,4 @@
-import { v2 as cloudinary } from "cloudinary";
-import { cloudinary_config } from "../config/config.js";
-
-cloudinary.config({
-  cloud_name: cloudinary_config.cloud_name,
-  api_key: cloudinary_config.api_key,
-  api_secret: cloudinary_config.api_secret,
-});
+import cloudinary from "../config/cloudinary.config.js";
 
 interface UploadResult {
   path: string;
