e45819a178eb8ac2a932f1cabdd729b78b569af3
bibekshah220 <bibekshah425@gmail.com>
2026-07-16 20:59:18 +0545

    feat: refactor product routes for improved structure and type safety

--- src/routes/product.routes.ts
+++ src/routes/product.routes.ts
@@ -1,5 +1,28 @@
-import { Router } from "express";
+import express, { Router } from "express";
+import {
+	create,
+	getAll,
+	getById,
+	update,
+	remove,
+} from "../controllers/product.controller.js";
+import { authenticate } from "../middlewares/auth.middleware.js";
+import { isAdmin } from "../middlewares/admin.middleware.js";
+import { uploader } from "../middlewares/uploader.middleware.js";
 
-const router = Router();
+const router: Router = express.Router();
+
+router.get("/", getAll);
+router.get("/:product_id", getById);
+
+router.post("/", authenticate(), isAdmin, uploader().array("images", 5), create);
+router.put(
+	"/:product_id",
+	authenticate(),
+	isAdmin,
+	uploader().array("images", 5),
+	update
+);
+router.delete("/:product_id", authenticate(), isAdmin, remove);
 
 export default router;
