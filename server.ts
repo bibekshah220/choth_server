5653f1059efb326aae121e167a2f5eaa7e61ed93
bibekshah220 <bibekshah425@gmail.com>
2026-07-16 20:56:09 +0545

    feat: add additional API routes for cart, wishlist, review, and dashboard functionalities

--- server.ts
+++ server.ts
@@ -8,6 +8,10 @@ import product_routes from "./src/routes/product.routes.js";
 import auth_routes from "./src/routes/auth.routes.js";
 import order_routes from "./src/routes/order.routes.js";
 import category_routes from "./src/routes/category.routes.js";
+import cart_routes from "./src/routes/cart.routes.js";
+import wishlist_routes from "./src/routes/wishlist.routes.js";
+import review_routes from "./src/routes/review.routes.js";
+import dashboard_routes from "./src/routes/dashboard.routes.js";
 
 import { errorHandler } from "./src/middlewares/error-handler.middleware.js";
 import { connect_db } from "./src/config/mongodb.config.js";
@@ -37,6 +41,10 @@ app.use("/api/product", product_routes);
 app.use("/api/auth", auth_routes);
 app.use("/api/order", order_routes);
 app.use("/api/category", category_routes);
+app.use("/api/cart", cart_routes);
+app.use("/api/wishlist", wishlist_routes);
+app.use("/api/review", review_routes);
+app.use("/api/dashboard", dashboard_routes);
 
 app.use((req, res, next) => {
   const message = `Cannot ${req.method} on ${req.originalUrl}`;
